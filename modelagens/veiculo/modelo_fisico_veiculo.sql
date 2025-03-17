0a24301602bc99c08b76a9f73d82e5ad882bf2f1
byanca lourenço <159471845+byancalourenco@users.noreply.github.com>
2025-03-17 10:30:50 -0300

    Add files via upload

---
+++ modelagens/veiculo/modelo_fisico_veiculo.sql
@@ -0,0 +1,53 @@
+-- Geração de Modelo físico
+-- Sql ANSI 2003 - brModelo.
+
+
+
+CREATE TABLE Cliente (
+CodCliente int PRIMARY KEY,
+nome varchar(100),
+telefone int,
+email varchar(100),
+dataNasci date,
+cep int,
+estado varchar(3),
+cidade varchar(100),
+bairro varchar(100),
+comple varchar(100),
+num int,
+rua varchar(100),
+rg int,
+cpf int
+)
+
+CREATE TABLE Carro (
+CodCarro int PRIMARY KEY,
+placa varchar(20),
+marca varchar(20),
+modelo varchar(50),
+cor varchar(10),
+chassi varchar(100),
+porte varchar(50),
+CodCliente int,
+FOREIGN KEY(CodCliente) REFERENCES Cliente (CodCliente)
+)
+
+CREATE TABLE Ocorrencia (
+CodOcorrencia int PRIMARY KEY,
+data date,
+hora time,
+rua varchar(100),
+bairro varchar(100),
+cidade varchar(100),
+descricao varchar(200),
+CodCarro int,
+CodTipo int,
+FOREIGN KEY(CodCarro) REFERENCES Carro (CodCarro)
+)
+
+CREATE TABLE Tipo (
+CodTipo int PRIMARY KEY,
+tipo varchar(50)
+)
+
+ALTER TABLE Ocorrencia ADD FOREIGN KEY(CodTipo) REFERENCES Tipo (CodTipo)
